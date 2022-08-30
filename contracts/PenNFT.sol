1d4e739a72a1f00feb0f5b2087da7b4869caf174
Hokuto Morita <han2210mh@gmail.com>
2022-08-30 22:33:00 +0900

    [add]アップグレード対応

--- contracts/PenNFT.sol
+++ contracts/PenNFT.sol
@@ -1,34 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.9;
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PenNFT is ERC721 {
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract PenNFT is Initializable, ERC721Upgradeable, ERC721BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
     string public constant TOKEN_URL = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    CountersUpgradeable.Counter private _tokenIdCounter;
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
     event NftMinted(uint256 indexed tokenId);
 
-    constructor() ERC721("PenNFT", "PNT") {}
 
-    function mintNft() public {
-        uint256 newItemId = _tokenIds.current();
-        console.log("newItemId: %s", newItemId);
-        _safeMint(msg.sender, newItemId);
-        emit NftMinted(newItemId);
-        console.log("NftMinted Event emited");
-        _tokenIds.increment();
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return TOKEN_URL;
+    function initialize() initializer public {
+        __ERC721_init("PenNFT", "PNT");
+        __ERC721Burnable_init();
+
+        // UUPSは、実装コントラクト側でアップグレードをおこないます
+        // そのため、アップグレードを実行できるのをOwnerに絞り込む必要があります
+        __Ownable_init();
+        __UUPSUpgradeable_init();
     }
+
+    function safeMint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        emit NftMinted(tokenId);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
 
     function getTokenCounter() public view returns (uint256) {
-        return _tokenIds.current();
+        return _tokenIdCounter.current();
     }
 }
